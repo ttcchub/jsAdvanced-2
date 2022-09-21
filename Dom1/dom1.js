014101203a3a8bf27eac527436c99f0e24646371
NNN <s2200198@TTCC-MacBook-Air.local>
2022-09-21 08:40:25 +0300

    new

---
+++ Dom1/dom1.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+Complete the renderShoppingList function such that it renders an <li> element
+for every item in the items array it receives.
+Also, the order of the items should be the same as the one in the array.
+So, the first item should show up first (at the top).
+
+
+/**
+ * @param {string[]} items
+ */
+
+const renderShoppingList = (items) => {
+  for (const item of items) {
+    document
+      .getElementById("shopping-list")
+      .insertAdjacentHTML("beforeend", `<li>${item}</li>`);
+  }
+};
+// Sample usage - do not modify
+const sampleList = ["Orange", "Banana", "Coffee", "Paper"];
+console.log(renderShoppingList(sampleList));
